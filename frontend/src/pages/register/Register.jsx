3eda4ccb82b3b025d78cd97dfb70edc2a0bf362a
Fabio Roberto Pereira Filho <112351137+Fasboyr@users.noreply.github.com>
2024-08-17 16:27:45 -0300

    Adicionando Validação de Senha
    Commit manual da pagina register devido à erros na tentativa de usar git add .
    
    Adicionando a validação de senha e reação em tempo real

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -1,15 +1,59 @@
-import React from "react";
+import React, { useState, useRef } from 'react';
 import "./Register.css";
-
 import { Card } from 'primereact/card';
 import { InputText } from 'primereact/inputtext';
 import { Password } from 'primereact/password';
 import { Button } from 'primereact/button';
+import { Message } from 'primereact/message';
 
 const Register = () => {
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState([]);
+    const typingTimeoutRef = useRef(null);  // Referência ao timeout para controle de inatividade
 
+    const validatePassword = (value) => {
+        const errors = [];
+        
+         if (value.length > 0) {
+            if (value.length < 6) {
+                errors.push("A senha deve ter no mínimo 6 caracteres.");
+            }
+            else if (!/[A-Z]/.test(value)) {
+                errors.push("A senha deve conter pelo menos uma letra maiúscula.");
+            }
+            else if (!/[a-z]/.test(value)) {
+                errors.push("A senha deve conter pelo menos uma letra minúscula.");
+            }
+            else if (!/[0-9]/.test(value)) {
+                errors.push("A senha deve conter pelo menos um número.");
+            }
+            else if (!/[^A-Za-z0-9]/.test(value)) {
+                errors.push("A senha deve conter pelo menos um caractere especial.");
+            }
+
+            setErrors(errors);
+        }
+    };
+
+    const onPasswordChange = (e) => {
+        const value = e.target.value;
+        setPassword(value);
+
+        if (typingTimeoutRef.current) {
+            clearTimeout(typingTimeoutRef.current);
+        }
+
+        if (value.length === 0) {
+            typingTimeoutRef.current = setTimeout(() => {
+                setErrors([]);
+            }, 1000);
+        } else {
+            typingTimeoutRef.current = setTimeout(() => {
+                validatePassword(value);
+            }, 1000);  // 1000 ms = 1 segundo
+        }
+    };
     return (
-
         <div className="register-grid">
             <Card title="Cadastro">
 
@@ -31,6 +75,8 @@ const Register = () => {
 
                 <div className="field">
                     <Password
+                        value={password}
+                        onChange={onPasswordChange}
                         id="password"
                         className="text-base text-color surface-overlay p-2appearance-none outline-none focus:border-primary w-full"
                         inputClassName="w-full p-2"
@@ -39,13 +85,17 @@ const Register = () => {
                         toggleMask
                     />
                 </div>
+
+                <div className="erros">
+                    {errors.length > 0 && errors.map((error, index) => (
+                        <Message key={index} severity="error" text={error} />
+                    ))}
+                </div>
+
                 <div className="register-options">
                     <Button label="Cancelar" size="small" />
                     <Button label="Cadastrar" size="small" />
                 </div>
-
-
-
             </Card>
         </div>
     );

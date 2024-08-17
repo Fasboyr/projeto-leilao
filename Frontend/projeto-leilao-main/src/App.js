5bf42054666d6d28034efb03c4fc4d446cff0add
Fabio Roberto Pereira Filho <112351137+Fasboyr@users.noreply.github.com>
2024-08-17 11:26:31 -0300

    Reorganizando arquivos em pasta e dando upload no backend

---
+++ Frontend/projeto-leilao-main/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import {BrowserRouter, Route, Routes} from  'react-router-dom';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import SimpleLayout from './components/SimpleLayout';
+import DefaultLayout from './components/DefaultLayout';
+
+
+function App() {
+  return (
+   <>
+    {/*<Header/>*/}
+      <BrowserRouter>
+        <Routes>
+            <Route path='/' element= {<DefaultLayout><Home/></DefaultLayout>}/>
+          <Route path='/login' element={<SimpleLayout><Login/></SimpleLayout>} />
+        </Routes>
+      </BrowserRouter>
+    {/*<Footer/>*/}
+   </>
+  );
+}
+
+export default App;
---
+++ Frontend/projeto-leilao-main/src/components/SimpleLayout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const SimpleLayout = ({children}) => {
+
+    return (
+            <>
+                {children}
+              
+            </>
+            );
+}
+
+export default SimpleLayout;

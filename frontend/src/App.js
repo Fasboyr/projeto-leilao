5e3341b7a33c76ca431045bd6f59fa8dee66d8a6
Fasboyr <fabio.filho200@hotmail.com>
2024-08-19 22:38:44 -0300

    Implementando logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,34 @@
 import './App.css';
-import {BrowserRouter, Route, Routes} from  'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import RecoverPassword from './pages/recoverpassword/RecoverPassword';
+import ChangePassword from './pages/changepassword/ChangePassword';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import SimpleLayout from './components/SimpleLayout';
 import DefaultLayout from './components/DefaultLayout';
+import PrivateRouter from './components/PrivateRouter';
 
 
 function App() {
   return (
-   <>
-    {/*<Header/>*/}
+    <>
+      {/*<Header/>*/}
       <BrowserRouter>
         <Routes>
-            <Route path='/' element= {<DefaultLayout><Home/></DefaultLayout>}/>
-          <Route path='/login' element={<SimpleLayout><Login/></SimpleLayout>} />
+          <Route element={<PrivateRouter />}>
+            <Route path='/' element={<DefaultLayout><Home /></DefaultLayout>} />
+            <Route path='/change' element={<SimpleLayout>< ChangePassword /></SimpleLayout>} />
+            <Route path='/recover' element={<SimpleLayout>< RecoverPassword /></SimpleLayout>} />
+          </Route>
+          <Route path='/login' element={<SimpleLayout><Login /></SimpleLayout>} />
+          <Route path='/register' element={<SimpleLayout><Register /></SimpleLayout>} />
         </Routes>
       </BrowserRouter>
-    {/*<Footer/>*/}
-   </>
+      {/*<Footer/>*/}
+    </>
   );
 }
 
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,32 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 
 import { Card } from 'primereact/card';
 import { InputText } from 'primereact/inputtext';
 import { Password } from 'primereact/password';
 import { Button } from 'primereact/button';
+import { useNavigate } from 'react-router-dom';
 
 
 
 
 
 const Login = () =>{
+    const [user, setUser] = useState({email:"", password:""});
+    const navigate = useNavigate();
 
-    return(
+    const handleChange = (input) =>{
+        setUser({...user, [input.target.name]:input.target.value});
+    }
+
+    const login = () => {
+        let token = "token do backend"
+        if(user.email == "fabio.filho2002@hotmail.com" && user.password == "123"){
+        localStorage.setItem("token", token);
+        localStorage.setItem("email", user.email);
+        navigate("/");
+        } else {
+            alert("User ou Senha Incorretos");
+        }
+    }
+     return(
         <div className="login-grid">
             <Card title= "Login">
 
                 <div class="field">
-                    <InputText className="input-field" placeholder="Digite aqui seu email" />
+                    <InputText onChange={handleChange} name="email" id="email" className="input-field" placeholder="Digite aqui seu email" />
 
                 </div>
                 <div class="field">
-                    <Password className="input-field" placeholder="Digite aqui sua senha" feedback={false} />
+                     <Password onChange={handleChange} name="password" id="password" className="input-field" placeholder="Digite aqui sua senha" feedback={false} />
 
                 </div>
            
                 <div className="button-container">
-                    <Button label="Login" className="login-button" />
+                    <Button onClick={login} label="Login" className="login-button" />
                 </div>
                 <div className="login-options">
                     <a href="#" className="sign-up">Cadastre-se</a>
